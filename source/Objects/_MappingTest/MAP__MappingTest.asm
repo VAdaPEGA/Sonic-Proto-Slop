a532d8e49174be23ba0ead82c89ed7c2965f1542
VAdaPEGA <50675500+VAdaPEGA@users.noreply.github.com>
2026-02-05 03:00:27 +0100

    Obj0F I got side-tracked, what was I doing again?

--- source/Objects/_MappingTest/MAP__MappingTest.asm
+++ source/Objects/_MappingTest/MAP__MappingTest.asm
@@ -16,34 +16,34 @@ Map_Obj0F:
 		dc.w word_B506-Map_Obj0F
 		dc.w word_B510-Map_Obj0F
 word_B47A:	dc.w 1			
-		dc.w	 0, 0, 0, 0
-word_B484:	dc.w 1	 	
-		dc.w	 1, 0, 0, 0
-word_B48E:	dc.w 1	
-		dc.w	 2, 0, 0, 0
-word_B498:	dc.w 1	
-		dc.w	 3, 0, 0, 0
-word_B4A2:	dc.w 1	
-		dc.w	 4, 0, 0, 0
-word_B4AC:	dc.w 1	
-		dc.w	 5, 0, 0, 0
-word_B4B6:	dc.w 1	
-		dc.w	 6, 0, 0, 0
-word_B4C0:	dc.w 1	
-		dc.w	 7, 0, 0, 0
-word_B4CA:	dc.w 1	
-		dc.w	 8, 0, 0, 0
-word_B4D4:	dc.w 1	
-		dc.w	 9, 0, 0, 0
-word_B4DE:	dc.w 1	
-		dc.w	$A, 0, 0, 0
-word_B4E8:	dc.w 1	
-		dc.w	$B, 0, 0, 0
-word_B4F2:	dc.w 1	
-		dc.b	$C, 0, 0, 0
-word_B4FC:	dc.w 1			
-		dc.b	$D, 0, 0, 0	
-word_B506:	dc.w 1		
-		dc.w	$E, 0, 0, 0
-word_B510:	dc.w 1			
-		dc.w	$F, 0, 0, 0
+		dc.w	0,	0,	0,	0
+word_B484:	dc.w 1	
+		dc.w	0,	1,	0,	0
+word_B48E:	dc.w 1
+		dc.w	0,	2,	0,	0
+word_B498:	dc.w 1
+		dc.w	0,	3,	0,	0
+word_B4A2:	dc.w 1
+		dc.w	0,	4,	0,	0
+word_B4AC:	dc.w 1
+		dc.w	0,	5,	0,	0
+word_B4B6:	dc.w 1
+		dc.w	0,	6,	0,	0
+word_B4C0:	dc.w 1
+		dc.w	0,	7,	0,	0
+word_B4CA:	dc.w 1
+		dc.w	0,	8,	0,	0
+word_B4D4:	dc.w 1
+		dc.w	0,	9,	0,	0
+word_B4DE:	dc.w 1
+		dc.w	0,	10,	0,	0
+word_B4E8:	dc.w 1
+		dc.w	0,	11,	0,	0
+word_B4F2:	dc.w 1
+		dc.b	0,	12,	0,	0
+word_B4FC:	dc.w 1
+		dc.b	0,	13,	0,	0	
+word_B506:	dc.w 1
+		dc.w	0,	14,	0,	0
+word_B510:	dc.w 1	
+		dc.w	0,	15,	0,	0
--- source/Objects/_MappingTest/_MappingTest.asm
+++ source/Objects/_MappingTest/_MappingTest.asm
@@ -1,39 +1,33 @@
 ; ===========================================================================
-; Object 0F - Mappings test?
+; 2 Player Mappings test
 ; ---------------------------------------------------------------------------
 
 Obj0F:
-		moveq	#0,d0
-		move.b	routine(a0),d0
-		move.w	Obj0F_Index(pc,d0.w),d1
-		jsr	Obj0F_Index(pc,d1.w)
-		bra.w	DisplaySprite
+	moveq	#0,d0
+	move.b	routine(a0),d0
+	move.w	@Index(pc,d0.w),d1
+	jmp	@Index(pc,d1.w)
 ; ===========================================================================
-Obj0F_Index:	dc.w loc_B416-Obj0F_Index
-		dc.w loc_B438-Obj0F_Index
-		dc.w loc_B438-Obj0F_Index
+	IndexStart
+	GenerateLocalIndex Init
+	GenerateLocalIndex Main
 ; ===========================================================================
-
-loc_B416:
-		addq.b	#2,routine(a0)
-		move.w	#$90,x_pixel(a0)
-		move.w	#$90,y_pixel(a0)
-		move.l	#Map_Obj0F,mappings(a0)
-		move.w	#$680,art_tile(a0)
-		bsr.w	Adjust2PArtPointer
-
-loc_B438:
-		move.b	(Ctrl_1_Press).w,d0
-		btst	#5,d0		; has C been pressed?
-		beq.s	loc_B44C	; if not, branch
+@Init:
+	addq.b	#2,routine(a0)
+	move.w	#128+16,x_pixel(a0)
+	move.w	#128+16,y_pixel(a0)
+	move.l	#Map_Obj0F,mappings(a0)
+	move.w	#VRAM_Dyn/$20,art_tile(a0)
+	bsr.w	Adjust2PArtPointer
+@Main:
+	move.b	(Ctrl_1_Press).w,d0
+	btst	#bitC,d0	; has C been pressed?
+	beq.s	@NoFrameChange	; if not, branch
 		addq.b	#1,mapping_frame(a0)	; increment mappings
 		andi.b	#$F,mapping_frame(a0)	; if above $F, reset
-
-loc_B44C:
-		btst	#4,d0		; has B been pressed?
-		beq.s	locret_B458	; if not, branch
+	@NoFrameChange:
+	btst	#bitB,d0	; has B been pressed?
+	beq.s	@DoNothing	; if not, branch
 		bchg	#0,(Two_player_mode+1).w	; set unused variable... which crashes the game?
-
-locret_B458:
-		rts
-; ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+	@DoNothing:
+		bra.w	DisplaySprite
